1d827e7749417f361ca43ac45e5b81cb652683aa
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-12 07:43:10 +0530

    Create variables.tf

---
+++ Modules/Service-catalog/variables.tf
@@ -0,0 +1,18 @@
+variable "portfolio_name" {}
+variable "portfolio_description" {}
+variable "provider_name" {}
+variable "distributor_name" {}
+variable "support_email" {}
+variable "support_url" {}
+variable "launch_role_arn" {}
+variable "iam_group_arn" {}
+
+variable "products" {
+  type = list(object({
+    product_name       : string,
+    product_owner      : string,
+    product_description: string,
+    artifact_version   : string,
+    template_url       : string,
+  }))
+}
