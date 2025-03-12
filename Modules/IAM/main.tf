21012960c9535b818bc2a93255b80c9507864613
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-12 15:36:52 +0530

    Update main.tf

--- Modules/IAM/main.tf
+++ Modules/IAM/main.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_policy" "service_catalog_policy" {
+/*resource "aws_iam_policy" "service_catalog_policy" {
   name        = "ServiceCatalogPolicy"
   description = "Policy for AWS Service Catalog to manage resources"
 
@@ -12,6 +12,70 @@ resource "aws_iam_policy" "service_catalog_policy" {
       }
     ]
   })
+}*/
+
+resource "aws_iam_policy" "ec2_servicecatalog_policy" {
+  name        = var.policy_name
+  description = "Policy to allow Service Catalog to create EC2 instances, access S3 artifacts, and manage resources"
+  policy      = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "ec2:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowS3AccessForServiceCatalogProvisioning",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:ExistingObjectTag/servicecatalog:provisioning": "true"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
+        },
+        {
+            "Sid": "AllowResourceGroupManagement",
+            "Effect": "Allow",
+            "Action": [
+                "resource-groups:CreateGroup",
+                "resource-groups:ListGroupResources",
+                "resource-groups:DeleteGroup",
+                "resource-groups:Tag",
+                "resource-groups:Untag"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowTaggingPermissions",
+            "Effect": "Allow",
+            "Action": [
+                "tag:GetResources",
+                "tag:GetTagKeys",
+                "tag:GetTagValues",
+                "tag:TagResources",
+                "tag:UntagResources"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
 }
 
 resource "aws_iam_role" "launch_role" {
@@ -32,5 +96,5 @@ resource "aws_iam_role" "launch_role" {
 
 resource "aws_iam_role_policy_attachment" "launch_role_policy_attachment" {
   role       = aws_iam_role.launch_role.name
-  policy_arn = aws_iam_policy.service_catalog_policy.arn
+  policy_arn = aws_iam_policy.ec2_servicecatalog_policy.arn
 }
