dd2a4f4c4c8b4ba77db1f6ecdd514aa3cdb29f50
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-12 11:19:21 +0530

    Update main.tf

--- Modules/IAM/main.tf
+++ Modules/IAM/main.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role" "launch_role" {
   name = "SCLaunch-HelloWorld"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement: [
+    Statement = [
       {
         Effect: "Allow",
         Principal: {
