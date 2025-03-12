ba3b9043944893e773bc93c3ae81cb0ff6ad1c4f
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-12 07:57:37 +0530

    Create variables.tf

---
+++ Infra/variables.tf
@@ -0,0 +1,42 @@
+variable "account_id" {
+  description = "The AWS account ID"
+}
+
+variable "portfolio_name" {
+  description = "The name of the Service Catalog portfolio"
+}
+
+variable "portfolio_description" {
+  description = "The description of the Service Catalog portfolio"
+}
+
+variable "provider_name" {
+  description = "The name of the provider"
+}
+
+variable "distributor_name" {
+  description = "The name of the distributor"
+}
+
+variable "support_email" {
+  description = "The support email for the products"
+}
+
+variable "support_url" {
+  description = "The support URL for the products"
+}
+
+variable "iam_group_arn" {
+  description = "The ARN of the IAM group with access to the portfolio"
+}
+
+variable "products" {
+  description = "The list of products to be added to the Service Catalog portfolio"
+  type = list(object({
+    product_name       : string,
+    product_owner      : string,
+    product_description: string,
+    artifact_version   : string,
+    template_url       : string,
+  }))
+}
