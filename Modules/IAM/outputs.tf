e388159112654acc1641cf24a72b261d944aef5b
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-12 07:26:02 +0530

    Create outputs.tf

---
+++ Modules/IAM/outputs.tf
@@ -0,0 +1,9 @@
+output "launch_role_arn" {
+  description = "The ARN of the launch role"
+  value       = aws_iam_role.launch_role.arn
+}
+
+output "launch_role_policy_arn" {
+  description = "The ARN of the policy attached to the launch role"
+  value       = aws_iam_policy.s3_resource_creation_and_artifact_access.arn
+}
