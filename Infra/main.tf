32db088cf1ca4796aaa25e94fd58cad6fb2a4894
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-12 07:55:12 +0530

    Create main.tf

---
+++ Infra/main.tf
@@ -0,0 +1,19 @@
+module "iam" {
+  source = "./IAM"
+
+  account_id = var.account_id
+}
+
+module "service_catalog" {
+  source = "./server-catalog"
+
+  portfolio_name        = var.portfolio_name
+  portfolio_description = var.portfolio_description
+  provider_name         = var.provider_name
+  distributor_name      = var.distributor_name
+  support_email         = var.support_email
+  support_url           = var.support_url
+  launch_role_arn       = module.iam.launch_role_arn
+  iam_group_arn         = var.iam_group_arn
+  products              = var.products
+}
