5662490485470f63786de380dac61e80e0802859
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-12 07:43:36 +0530

    Create outputs.tf

---
+++ Modules/Service-catalog/outputs.tf
@@ -0,0 +1,22 @@
+output "portfolio_id" {
+  description = "The ID of the Service Catalog portfolio"
+  value       = aws_servicecatalog_portfolio.portfolio.id
+}
+
+output "product_ids" {
+  description = "The IDs of the Service Catalog products"
+  value       = [for product in aws_servicecatalog_product.products : product.id]
+}
+
+output "portfolio_product_associations" {
+  description = "The associations between the portfolio and products"
+  value       = [for assoc in aws_servicecatalog_portfolio_product_association.product_association : {
+    portfolio_id = assoc.portfolio_id,
+    product_id   = assoc.product_id
+  }]
+}
+
+output "portfolio_access_group" {
+  description = "The ARN of the IAM group with access to the portfolio"
+  value       = aws_servicecatalog_principal_portfolio_association.portfolio_access_group.principal_arn
+}
